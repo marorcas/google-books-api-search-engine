9cd04ef75c56095d76b6f4c3eb617e122c5cd14e
marorcas <ortzmc@gmail.com>
2024-07-18 14:11:00 +1000

    Implement basic book modal

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react"
+
 import styles from "./BookCard.module.scss"
 
 const BookCard = ({ book }) => {
+  const [showBookModal, setShowBookModal] = useState(false)
+
+  const openBookModal = () => {
+    setShowBookModal(true)
+  }
+
+  const closeBookModal = () => {
+    setShowBookModal(false)
+  }
+
   return (
 
     <article className={styles.BookCover}>
-      <div className={styles.BookInfo}>
+      <div className={styles.BookInfo} onClick={openBookModal}>
         <h2>{book.volumeInfo.title}</h2>
-        <h4>{book.volumeInfo.authors}</h4>
+        <h4>{book.volumeInfo.authors.join(", ")}</h4>
       </div>
+
+      {showBookModal && (
+        <div className={styles.BookModal}>
+          <button onClick={closeBookModal}>Close</button>
+        </div>
+      )}
     </article>
   )
 }
