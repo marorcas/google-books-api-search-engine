d317e0396782906601b805f03c125fe5542dd4b8
marorcas <ortzmc@gmail.com>
2024-07-19 15:04:28 +1000

    Create design for book cards

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -14,25 +14,48 @@ const BookCard = ({ book }) => {
   }
 
   return (
+    <>
+      <article className={styles.Book} onClick={openBookModal}>
+        <div className={styles.LeftPage}>
+          {book.volumeInfo.imageLinks ? (
+            <img src={book.volumeInfo.imageLinks.thumbnail}
+              className={styles.Cover} />
+          ) : (
+            <img src="../../assets/placeholder.png" />
+          )}
+        </div>
 
-    <article className={styles.BookCover}>
-      <div className={styles.BookInfo} onClick={openBookModal}>
-        {book.volumeInfo.imageLinks ? (
-          <img src={book.volumeInfo.imageLinks.thumbnail} />
-        ) : (
-          <img src="../../assets/placeholder.png" />
-        )}
-        <p className={styles.BookTitle}>{book.volumeInfo.title}</p>
-        {/* <h2>{book.volumeInfo.title}</h2>
-        <h4>{book.volumeInfo.authors.join(", ")}</h4> */}
-      </div>
+        <div className={styles.RightPage}>
+          <p className={styles.Title}>{book.volumeInfo.title}</p>
+          <p className={styles.Author}>{book.volumeInfo.authors.join(", ")}</p>
+        </div>
+      </article>
 
       {showBookModal && (
-        <div className={styles.BookModal}>
+        <div className={styles.Modal}>
           <button onClick={closeBookModal}>Close</button>
         </div>
       )}
-    </article>
+    </>
+
+    // <article className={styles.BookCover}>
+    //   <div className={styles.BookInfo} onClick={openBookModal}>
+        // {book.volumeInfo.imageLinks ? (
+        //   <img src={book.volumeInfo.imageLinks.thumbnail} />
+        // ) : (
+        //   <img src="../../assets/placeholder.png" />
+        // )}
+        // <p className={styles.BookTitle}>{book.volumeInfo.title}</p>
+    //     {/* <h2>{book.volumeInfo.title}</h2>
+    //     <h4>{book.volumeInfo.authors.join(", ")}</h4> */}
+    //   </div>
+
+      // {showBookModal && (
+      //   <div className={styles.BookModal}>
+      //     <button onClick={closeBookModal}>Close</button>
+      //   </div>
+      // )}
+    // </article>
   )
 }
 
