fe54a3a4badf4ec5ea8e73eff8d838b1b3acdc1e
marorcas <ortzmc@gmail.com>
2024-07-21 18:23:05 +1000

    Add pagination

--- src/components/BooksList/BooksList.jsx
+++ src/components/BooksList/BooksList.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
+
 import styles from "./BooksList.module.scss"
 
 import BookCard from "../BookCard/BookCard"
 
 const BooksList = ({ booksList }) => {
-    console.log(booksList.length)
+    const [currentPage, setCurrentPage] = useState(1);
+    const booksPerPage = 6;
+  
+    const lastIndex = currentPage * booksPerPage;
+    const firstIndex = lastIndex - booksPerPage;
+    const currentBooks = booksList.slice(firstIndex, lastIndex);
+  
+    const paginate = (pageNumber) => {
+      setCurrentPage(pageNumber)
+    }
+
     return (
-        <div className={styles.BooksList}>
-            {booksList.map((book) => 
-                <BookCard key={book.id} book={book} />
-            )}
-        </div>
+        <section>
+            <div className={styles.BooksList}>
+                {currentBooks.map((book) => 
+                    <BookCard key={book.id} book={book} />
+                )}
+            </div>
+
+            <div className={styles.Pagination}>
+                <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
+                    Back
+                </button>
+
+                <button onClick={() => paginate(currentPage + 1)} disabled={lastIndex >= booksList.length}>
+                    Next
+                </button>
+            </div>
+        </section>
     )
 }
 
